bc802c284f8bacdd7da9115668c2501f83dbd8ae
Casey Johnson <johnsoncm3@gmail.com>
2021-05-13 17:04:35 -0400

    added seed.sql data

--- employeeTracker.js
+++ employeeTracker.js
@@ -22,18 +22,89 @@ const connection = mysql.createConnection({
     database: 'employee_trackerDB',
 
 });
-connection.connect((err) =>{
-    if (err) throw err;
-    console.log(`connected as ${connection.threadId}`);
-   start();
- 
-});
 
 //cool title graphics
 figlet(`Employee Tracker` , (err, result) => {
     console.log(err || result);
 });
 
+connection.connect((err) =>{
+    if (err) throw err;
+    console.log(`connected as ${connection.threadId}`);
+   start();
+   getDepartments();
+   getRoles();
+   getManagers();
+   getEmployees();
+ 
+});
+
+
+
+// first question asking user if they want to add, view, update, or exit
+
+function start() {
+    inquirer
+        .prompt({
+            name: 'choices',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['ADD' , 'VIEW' , 'UPDATE' , 'EXIT']
+        })
+        .then(function(answer){
+            if (answer.choices === 'ADD') {
+                addSomething();
+            }
+            else if (answer.choices === 'VIEW') {
+                viewSomething();
+            }
+            else if (answer.choices === 'UPDATE') {
+                updateSomething();
+            }
+            else if (answer.choices === 'DELETE') {
+                deleteSomething();
+            }
+            else if (answer.choices === 'EXIT') {
+                figlet(`Goodbye!` , (err, result) => {
+                    console.log(err || result);
+                });
+                connection.end();
+            }
+            else {
+                connection.end();
+            }
+        })
+    }
+
+
+    getRoles = () => {
+        connection.query('SELECT id, role_title FROM roles' , (err, res) => {
+            if (err) throw err;
+            roles = res;
+        })
+    },
+    
+    getDepartments = () => {
+        connection.query('SELECT id, dept_name FROM department', (err, res) => {
+            if (err) throw err;
+            departments = res;
+         })
+    },
+    
+    getManagers = () => {
+        connection.query("SELECT id, first_name, last_name , CONCAT_WS('' , first_name, last_name) AS managers FROM employee" , (err, res) => {
+            if (err) throw err;
+            managers = res;
+        })
+    }
+    
+    getEmployees = () => {
+        connection.query("SELECT id, CONCAT_WS(' ' , first_name, last_name) AS Employee_Name from employee" , (err, res) => {
+            if (err) throw err;
+            employees = res;
+        })
+    };
+    
 
 // When 'add' option is chosen the user is given option to add dept, role, or employee
 function addSomething() {
@@ -58,25 +129,109 @@ function addSomething() {
             figlet('Goodbye!',(err, result) =>{
                 console.log(err || result)
             });
-                connection,end();
+                connection.end();
         }else {
             connection.end();
         }
+
+
+    }
+)
+}
+
+//When user chooses to add a dept. they are given prompts to enter more information
+addDepartment = () => {
+    inquirer.prompt([
+        {
+            name: 'dept_name',
+            type: 'input',
+            message: 'what department would you like to add'
+        }
+    ]).then(function(answer){
+        console.log('answer2' ,answer)
+        connection.query(`INSERT INTO department (dept_name) VALUES ('${answer.dept_name}')`, (err, res) => {
+            if (err) throw err;
+            console.log('1 new department added: ' + answer.dept_name);
+            console.log('answer' , answer);
+            getDepartments();
+            start();         
+
+        })
+        // .then(function(answer){
+        //     connection.query(`INSERT ALL departments (dept_name) VALUES (${answer.dept_name})` , (err, res) => {
+        //         if(err) throw err;
+
+        //     })
+        // })
+        //query to database to get all departments then response is push to departments array
     })
 }
 
-addEmployee = () => {
-    // getRoles();
-    // getManagers();
-    // let roleOptions = [];
-    // for (i = 0; i < roles.length; i++) {
-    //     roleOptions.push(Object(roles[i]));
-    // };
-    // let managerOptions = [];
-    // for (i = 0; i < managers.length; i++) {
-    //     managerOptions.push(Object(managers[i]));
-    // };
 
+ // When add role option is chosen, the user is prompted to answer questions about the role
+ addRole = () =>{
+    let departmentOptions = [];
+    for (i = 0; i < departments.length; i++) {
+       
+        
+        departmentOptions.push(Object(departments[i]));
+    }
+    console.log('department options' , departmentOptions);
+
+    inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What role would you like to add?'
+
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'What is the salary requirement for this position?'
+        },
+        {
+            name: 'dept_id',
+            type: 'list',
+            message: 'What is the department for this position?',
+            choices: departmentOptions
+            
+
+        },
+  
+    ]).then(function(answer){
+        for (i = 0; i < departmentOptions.length; i++) {
+            if (departmentOptions[i].name === answer.department_id){
+                department_id = departmentOptions[i].id
+            }
+        }
+
+        connection.query(`INSERT INTO roles (role_title, salary, dept_id) VALUES ('${answer.role_title}', '${answer.salary}', '${dept_id}')`, (err, res) => {
+            if (err) throw err;
+
+            console.log('1 new role added: ' + answer.title);
+            getRoles();
+            start();
+
+        })
+    })
+}
+
+
+
+
+
+addEmployee = () => {
+    getRoles();
+    getManagers();
+    let roleOptions = [];
+    for (i = 0; i < roles.length; i++) {
+        roleOptions.push(Object(roles[i]));
+    };
+    let managerOptions = [];
+    for (i = 0; i < managers.length; i++) {
+        managerOptions.push(Object(managers[i]));
+    };
     inquirer.prompt([
         {
             name: "first_name",
@@ -130,160 +285,179 @@ addEmployee = () => {
         connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (${answer.first_name} , ${answer.last_name}, ${answer.role_id}, ${answer.manager_id})` , (err, res) => {
             if (err) throw err;
             console.log('1 new employee added: ' + answer.first_name + "" + answer.last_name);
-            // getEmployees();
-            // start();
-        }
-    )
-},  
-
-// When add role option is chosen, the user is prompted to answer questions about the role
-addRole = () =>{
-    let departmentOptions = [];
-    for (i = 0; i < departments.length; i++) {
-        console.log("test");
-        console.log("departments" ,departments)
-        departmentOptions.push(Object(departments[i]));
-    }
-    console.log('department options' , departmentOptions);
-    inquirer.prompt([
-        {
-            name: 'title',
-            type: 'input',
-            message: 'What role would you like to add?'
-
-        },
-        {
-            name: 'salary',
-            type: 'input',
-            message: 'What is the salary requirement for this position?'
-        },
-        {
-            name: 'department_id',
-            type: 'list',
-            message: 'What is the department for this position?',
-            choices: departmentOptions
-
-        },
-    ]).then(function(answer){
-        for (i = 0; i < departmentOptions.length; i++) {
-            if (departmentOptions[i].name === answer.department_id){
-                department_id = departmentOptions[i].id
-            }
-        }
-
-        connection.query(`INSERT INTO employeeRole (role_title, salary, dept_id)); VALUES ('${answer.role_title}', '${answer.salary}', ${dept_id})`, (err, res) => {
-            if (err) throw err;
-
-            console.log('1 new rold added: ' + answer.title);
-            getRoles();
+            getEmployees();
             start();
-
         })
     })
-},
-
-
-
-getDepartments = () => {
-    connection.query('SELECT id, dept_name FROM department', (err, res) => {
-        if (err) throw err;
-        departments = res;
-        console.log(departments);
-    })
-},
-
-//When user chooses to add a dept. they are given prompts to enter more information
-addDepartment = () => {
-    inquirer.prompt([
-        {
-            name: 'dept_name',
-            type: 'input',
-            message: 'what department would you like to add'
+    };
+    
+    function viewSomething(){
+        inquirer
+        .prompt[(
+            {
+            name:'viewChoice',
+            type: 'list',
+            message: 'What would you like to view?',
+            choices: ['Departments', 'Roles', 'Employees', 'Exit']
+    
         }
-    ]).then(function(answer){
-        console.log('answer2' ,answer)
-        connection.query(`INSERT INTO department (dept_name) VALUES ('${answer.dept_name}')`, (err, res) => {
-            if (err) throw err;
-            console.log('1 new department added: ' + answer.dept_name);
-            console.log('answer' , answer);
-            getDepartments();
-            start();         
+    )].then(answer => {
+        
+        if (answer.viewChoice === "Departments") {
+           return viewDepartments();
+        }
+        else if (answer.viewChoice === "Roles") {
+           return viewRoles();
+        }
+        else if(answer.viewChoice === "Employees"){
+           return viewEmployees();
+        }
+        else if(answer.viewChoice === "Exit"){
+         return   figlet('Goodbye!' , (err, result) => {
+                console.log(err || result);
+            })
+        }else {
+            connection.end();
+        }
 
-        })
-        // .then(function(answer){
-        //     connection.query(`INSERT ALL departments (dept_name) VALUES (${answer.dept_name})` , (err, res) => {
-        //         if(err) throw err;
-
-        //     })
-        // })
-        //query to database to get all departments then response is push to departments array
     })
-},
+};
+
 
 //When  user chooses the 'View' option, they are gien the option to view dept, roles, employees, or exit
-function viewSomething(){
-    inquirer
-    .prompt({
-        name:'viewChoices',
-        type: 'list',
-        message: 'What would you like to view?',
-        choices: ['Departments', 'Roles', 'Employees', 'Exit']
 
+
+viewDepartments = () =>{
+    connection.query("SELECT * FROM department" , (err, res) => {
+        if (err) throw err;
+        console.log(err || result)
+    });
+
+    cTable(res);
+    start();
+
+}
+
+viewRoles = () => {
+    connection.query("SELECT r.id, r.role_title, r.salary, r.dept_id AS Department_Name FROM roles AS r INNER JOIN department AS d ON r.dept_id = d.id" , (err, res) =>{
+        if (err) throw err;
+        console.log(err || result);
+    });
+
+    cTable(res);
+    start();
+
+}
+
+viewEmployees = () => {
+    connection.query("SELECT e.id, e.first_name, e.last_name, d.name AS department, r.role_title, r.salary, CONCAT_WS('' , m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN roles r ON e.role_id = r.id INNER JOIN department d ON r.dept_id = d.id ORDER BY e.id ASC" , (err, res)=>{
+        if (err) throw err;
+        console.log(err || result);
     })
-},
+    cTable(res);
+    start();
+}
 
 //When user choose 'update' option they are prompted to choose what to update
 function updateSomething(){
     inquirer
-    .prompt({
+    .prompt([{
         name: 'update',
         type: 'list',
         message: 'What would you like to update?',
         choices: ['Employee Roles', 'Employee Managers' , 'Exit']
-    })
-},
-// when a use chooses to delete something, they are prompted to choose what to delete
-function deleteSomething(){
-    inquirer
-    .prompt({
-        name: 'delete',
-        type: 'list',
-        message: 'What would you like to delete?',
-        choices: ['Delete Department', 'Delete Employee', 'Delete Role', 'Exit']
-    });
-})}
-
-// first question asking user if they want to add, view, update, or exit
-
-function start() {
-    inquirer
-        .prompt({
-            name: 'choices',
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: ['ADD' , 'VIEW' , 'UPDATE' , 'EXIT']
-        })
-        .then(function(answer){
-            if (answer.choices === 'ADD') {
-                addSomething();
-            }
-            else if (answer.choices === 'VIEW') {
-                viewSomething();
-            }
-            else if (answer.choices === 'UPDATE') {
-                updateSomething();
-            }
-            else if (answer.choices === 'DELETE') {
-                deleteSomething();
-            }
-            else if (answer.choices === 'EXIT') {
-                figlet(`Goodbye!` , (err, result) => {
-                    console.log(err || result);
-                });
-                connection.end();
-            }
-            else {
-                connection.end();
-            }
-        })
     }
+]).then(answer => {
+    if (answer.update === "Employee Roles") {
+        updateEmployeeRole();
+    }else if (answer.update === "Employee Managers"){
+        updateEmployeeManager();
+    }else if(answer.update === "Exit"){
+        figlet("Goodbye!" , (err, result) => {
+            console.log(err || result);
+        });
+        connection.end();
+    }else{
+        connection.end();
+    }
+})
+}
+
+
+updateEmployeeRole = () => {
+    let employeeOptions = [];
+
+    for(var i = 0; i < employees.length; i++) {
+        employeeOptions.push(Object(employees[i]));
+    }
+    inquirer.prompt([
+        {
+            name: "updateRole",
+            type: "list",
+            message: "Which employee's role do you want to update?",
+            choices: function () {
+                var choiceArray = [];
+                for(var i = 0; i < employeeOptions.length; i++){
+                    choiceArray.push(employeeOptions[i].Employee_Name);
+                }
+                return choiceArray;
+            }
+        }
+    ]).then(answer => {
+        let roleOptions =[];
+        for (var i = 0; i < roles.length; i ++){
+            roleOptions.push(Object(roles[i]));
+        };
+        for (i=0; i < employeeOptions.length; i++){
+            employeeSelected = employeeOptions[i].id
+        }
+    },
+    inquirer.prompt ([
+        {
+            name: "newRole",
+            type: "list",
+            message: "Select new role:",
+            choices: function () {
+                var choiceArray = [];
+                let roleOptions =[];
+                for (var i = 0; i < roleOptions.length; i++){
+                    choiceArray.push(roleOptions[i].title)
+                }
+                return choiceArray;
+            }
+
+        }
+    ]).then(answer => {
+        for(var i = 0; i < roleOptions.length; i++){
+            if(answer.newRole === roleOptions[i].title) {
+                newChoice = roleOptions[i].id
+                connection.query(`UPDATE employee SET role_id = ${newChoice} WHERE id = ${employeeSelected}`) , (err, res) =>{
+                    if (err) throw err;
+                }
+            }
+        }
+        console.log("Role has been updated!");
+        getEmployees();
+        getRoles();
+        start();
+    })
+    
+    )
+
+}
+// when a use chooses to delete something, they are prompted to choose what to delete
+// function deleteSomething(){
+//     inquirer
+//     .prompt({
+//         name: 'delete',
+//         type: 'list',
+//         message: 'What would you like to delete?',
+//         choices: ['Delete Department', 'Delete Employee', 'Delete Role', 'Exit']
+//     });
+// }
+
+
+   
+   
+
+
--- seed.sql
+++ seed.sql
@@ -1,1 +1,13 @@
-// this file pre-populates database =
+
+INSERT INTO employee (first_name, last_name) values ('Casey', 'Johnson');
+INSERT INTO employee (first_name, last_name) values ('Roberto', 'Jamora');
+INSERT INTO employee (first_name, last_name) values ('Zack', 'Johnson');
+INSERT INTO employee (first_name, last_name) values ('Patrick', 'Johnson');
+INSERT INTO employee (first_name, last_name) values ('Sandra', 'Johnson');
+
+
+INSERT INTO role_title (role_title, salary) values ('Artist', 55000);
+INSERT INTO role_title (role_title, salary) values ('Executive Director', 150000);
+INSERT INTO role_title (role_title, salary) values ('Accountant', 75000);
+INSERT INTO role_title (role_title, salary) values ('CFO', 100000);
+INSERT INTO role_title (role_title, salary) values ('Graphic Designer', 55000);
